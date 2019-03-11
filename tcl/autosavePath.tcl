06866d2ac4e2b707424f9d737c2f0ba9210c6143
Steve Hawken <steve@primaryvfx.com>
2019-03-11 11:02:34 +0000

    added comment

--- tcl/autosavePath.tcl
+++ tcl/autosavePath.tcl
@@ -1,1 +1,3 @@
+## autosavePath expression to copy and paste into nuke prefs
+
 [if {[value root.name] !=""} {return  [file mkdir c:/Users/Admin/AppData/Local/nuke/autosaves/[lindex [split [value root.name] "/ "] 3 ]]c:/Users/Admin/AppData/Local/nuke/autosaves/[lindex [split [value root.name] "/"] 3]/[file tail [value root.name].autosave]} {return [firstof [value root.name] [getenv NUKE_TEMP_DIR]/].autosave}] 
